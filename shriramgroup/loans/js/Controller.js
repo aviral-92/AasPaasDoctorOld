b5d8d6de34bc2530657004b0397852d61f76bf4f
Aviral-92 <aviralmittal2012@gmail.com>
2017-07-07 08:39:49 +0530

    Initial Commit

---
+++ shriramgroup/loans/js/Controller.js
@@ -0,0 +1,22 @@
+var app = angular.module("myApp",['ngSanitize', 'ngMaterial', 'ui.bootstrap']);
+
+app.controller("myController", function($scope){
+   
+    // initializing the time Interval
+    $scope.firstSliderInterval = 3000;
+
+    // Initializing slide array
+    $scope.slides = [{
+        image: '/images/1.jpg',
+        text: 'We have to connect every Indian to good medical facility. We are working for the people  not for money.'
+    }, {
+        image: '/images/about.jpg',
+        text: 'We have to connect every Indian to good medical facility. We are working for the people  not for money.'
+    }, {
+        image: '/images/about.jpg',
+        text: 'We have to connect every Indian to good medical facility. We are working for the people  not for money.'
+    }, {
+        image: '/images/about.jpg',
+        text: 'We have to connect every Indian to good medical facility. We are working for the people  not for money.'
+    }];
+});
